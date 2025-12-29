78cf450a95b6832fa2e8a65a910238e7781c7001
ArtAdemiUP <art.ademi@student.uni-pr.edu>
2025-12-29 09:28:51 +0100

    Add API configuration and default cities

---
+++ config.js
@@ -0,0 +1,14 @@
+export const API_CONFIG = {
+    API_KEY: 'f00c38e0279b7bc85480c3fe775d518c',
+    BASE_URL: 'https://api.openweathermap.org/data/2.5',
+    ICON_URL: 'https://openweathermap.org/img/wn'
+};
+
+export const DEFAULT_CITIES = [
+    'Tirana',
+    'Pristina',
+    'Skopje',
+    'London',
+    'Paris',
+    'New York'
+];
